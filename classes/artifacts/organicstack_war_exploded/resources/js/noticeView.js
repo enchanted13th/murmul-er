6b838ad76361be1d2ed0a6def8bf9c56037b0bdd
seokju2ng <seokju2ng@naver.com>
2019-08-10 19:46:02 +0900

    [ISSUE-6] classes folder 날리기
    날렸습니다
    
    -김석중

--- classes/artifacts/organicstack_war_exploded/resources/js/noticeView.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('#back').click(function () {
-        history.back();
-    })
-    $('#btnNotice').css('border-bottom', '6px solid #b6e2f8');
-})
--- classes/artifacts/organicstack_war_exploded/resources/js/terms.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$('#btnTerms').css('border-bottom', '6px solid #b6e2f8');
-})
