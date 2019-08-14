09052e32f2e93b29c7b2dfa3a92c3b792f4ed7ed
kim <rmdk6qjs@naver.com>
2019-08-14 18:27:25 +0900

    [ISSUE-32] 계약서 service, DAO, mapper 설계 및 구현
    -계약서 등록 관련 서비스, dao, mapper 설계

---
+++ src/test/java/com/murmuler/organicstack/ContractTest.java
@@ -0,0 +1,32 @@
+package com.murmuler.organicstack;
+
+import com.murmuler.organicstack.dao.ContractDAO;
+import com.murmuler.organicstack.vo.ContractVO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.sql.Date;
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "file:web/WEB-INF/dispatcher-servlet.xml",
+        "file:web/WEB-INF/mybatis-config.xml"
+})
+@WebAppConfiguration
+public class ContractTest {
+    @Autowired
+    private ContractDAO contractDAO;
+
+    @Test
+    public void insertContractTest() {
+        int res = contractDAO.insertContract(new ContractVO(7, 247, 85, 84, "", new Date(1), "contract/10/format.png", ""));
+
+        assertTrue("실패",res == 1);
+    }
+}
