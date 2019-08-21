8034cfd6e670dc3f89c7fc9786631b02a3da69d5
lecto17 <rmdk6qjs@naver.com>
2019-08-21 16:50:21 +0900

    [ISSUE-80] 안드로이드용 로그인 페이지 만들기
    - 아이디, 비밀번호 중 하나라도 입력 없을 시 입력하도록 요구
    - 입력 성공, 실패일 경우 swal 적용

---
+++ web/resources/js/mobile/m_login.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    let wh = $(window).height();
+    let ph = 622;
+    let top = (wh - ph) / 2;
+    $('body').find('.wrapLogin').children('div').css('margin-top', top);
+
+    $('.loginBtn').click(function(){
+        $.sendData();
+    })
+});
+
+$.sendData = function(){
+    if(!$.checkValid())
+        return false;
+
+    $.ajax('/member/login',{
+        type: 'POST',
+        data:{
+            id: $('#inputId').val(),
+            pwd: $('#inputPwd').val()
+        }
+    }).then(function(data, status){
+        if(status === "success"){
+            switch (data.loginResult) {
+                case "SUCCESS":
+                    Swal.fire({
+                        type: 'success',
+                        title: data.nick + '님 안녕하세요.',
+                        showConfirmButton: false,
+                        timer: 1500
+                    }).then(function(){
+                        if (flag === "mypage") location.href = "/mypage/recent";
+                        else if (flag === "talk") { $.showTalk(); location.href="";}
+                        else location.href = "";
+                    })
+                    break;
+                case "WRONG_ID":
+                    Swal.fire('로그인 실패', '존재하지 않는 아이디입니다.', 'error')
+                        .then(function() {
+                            Swal.close();
+                            $('#inputId').focus();
+                        })
+                    break;
+                case "WRONG_PWD":
+                    Swal.fire('로그인 실패', '비밀번호가 틀렸습니다.' ,'error')
+                        .then(function(){
+                            $('#inputPwd').focus();
+                        })
+                    break;
+            }
+        }
+    });
+}
+
+$.checkValid = function() {
+    if($('#inputId').val() === ''){
+       Swal.fire('', '아이디를 입력하세요.','warning');
+        $('#inputId').focus();
+        return false;
+    }
+
+    if($('#inputPwd').val() == ''){
+        Swal.fire('', '비밀번호를 입력하세요.','warning');
+        $('#inputPwd').focus();
+        return false;
+    }
+
+    return true;
+}
