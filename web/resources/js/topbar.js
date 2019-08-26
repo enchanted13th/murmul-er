4b7d3be3d33ada3b8319d79d5fd32a7fea877b7e
seokju2ng <ehddnwnd@naver.com>
2019-08-26 22:47:20 +0900

    [ISSUE-117] topbar에 defend()
    - string text를 스크립트로부터 막는 함수

--- web/resources/js/topbar.js
+++ web/resources/js/topbar.js
@@ -503,3 +503,14 @@ $.fn.clickCancelBtn = function (url) {
         })
     })
 }
+
+function defend(value) {
+    value = value+"";
+    value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    value = value.replaceAll("\\(", "& #40;").replaceAll("\\)", "& #41;");
+    value = value.replaceAll("'", "& #39;");
+    value = value.replaceAll("eval\\((.*)\\)", "");
+    value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+    value = value.replaceAll("script", "");
+    return value;
+}
